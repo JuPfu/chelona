2fe34d4120d0d5c3ad84fa71d7522d2330695b15
jp <juergen.pfundt@gmail.com>
2015-05-18 20:25:28 +0200

    Allowed Whitespace in Turtle after ^^ see W3C home > Mailing lists > Public > public-rdf-comments@w3.org > January 2015

--- src/main/scala/org/chelona/ChelonaParser.scala
+++ src/main/scala/org/chelona/ChelonaParser.scala
@@ -170,7 +170,7 @@ class ChelonaParser(val input: ParserInput, val output: Writer, validate: Boolea
   }
 
   def ws = rule {
-    quiet(anyOf(" \t").* ~ ('#' ~ noneOf("\n\r").*).? ~ anyOf(" \n\r\t").*)
+    quiet(anyOf(" \n\r\t").* ~ ('#' ~ noneOf("\n\r").*).? ~ anyOf(" \n\r\t").*)
   }
 
   //[1] turtleDoc 	::= 	statement*
@@ -313,7 +313,7 @@ class ChelonaParser(val input: ParserInput, val output: Writer, validate: Boolea
 
   //[128s] RDFLiteral 	::= 	String (LANGTAG | '^^' iri)?
   def rdfLiteral = rule {
-    string ~ (LANGTAG | str("^^") ~ iri).? ~ ws ~> ASTRdfLiteral
+    string ~ (ws ~ LANGTAG | str("^^") ~ ws ~ iri).? ~ ws ~> ASTRdfLiteral
   }
 
   //[144s] LANGTAG 	::= 	'@' [a-zA-Z]+ ('-' [a-zA-Z0-9]+)*
