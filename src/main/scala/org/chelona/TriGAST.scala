bff1eb73e400ee55991d37bbfded5b3aa0f54c88
jp <juergen.pfundt@gmail.com>
2015-07-13 21:23:25 +0200

    renaming trig to triG hassle

---
+++ src/main/scala/org/chelona/TriGAST.scala
@@ -0,0 +1,42 @@
+/*
+* Copyright (C) 2014-2015 Juergen Pfundt
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.chelona
+
+trait TriGAST extends TurtleAST with RDFASTType {
+
+  type TriGType = TriGAST
+
+  case class ASTTrigDoc(rule: TurtleType) extends TriGAST
+
+  case class ASTBlock(rule: TurtleType) extends TriGAST
+
+  case class ASTLabelOrSubjectBlock(labelOrSubject: ASTLabelOrSubject, wrappedGraph: ASTWrappedGraph) extends TriGAST
+
+  case class ASTTriplesOrGraph(labelOrSubject: TurtleType, rule: TurtleType) extends TriGAST
+
+  case class ASTTriple2BlankNodePropertyList(blankNodePropertyList: TurtleType, predicateObjectList: Option[TurtleType]) extends TriGAST
+
+  case class ASTTriple2Collection(collection: TurtleType, predicateObjectList: TurtleType) extends TriGAST
+
+  case class ASTWrappedGraph(triplesBlock: Option[TurtleType]) extends TriGAST
+
+  case class ASTTriplesBlock(triples: Seq[TurtleType]) extends TriGAST
+
+  case class ASTLabelOrSubject(rule: TurtleType) extends TriGAST
+}
+
+object TriGAST extends TriGAST
---
+++ src/main/scala/org/chelona/TriGParser.scala
@@ -0,0 +1,99 @@
+/*
+* Copyright (C) 2014-2015 Juergen Pfundt
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.chelona
+
+import java.io.Writer
+
+import org.chelona.EvalTriG.TrigTuple
+import org.chelona.TriGParser.QuadAST
+
+import org.parboiled2._
+
+import scala.language.implicitConversions
+
+object TriGParser extends TriGAST {
+
+  def apply(input: ParserInput, output: Writer, validate: Boolean = false, basePath: String = "http://chelona.org", label: String = "") = {
+    new TriGParser(input, output, validate, basePath, label)
+  }
+
+  def trigWriter(bo: Writer)(triple: List[TrigTuple]): Int = {
+    triple.map(t ⇒ bo.write(t.s + " " + t.p + " " + t.o + (if (t.g != "") " " + t.g + " .\n" else " .\n"))).length
+  }
+
+  sealed trait QuadAST extends TriGAST
+
+}
+
+class TriGParser(input: ParserInput, output: Writer, validate: Boolean = false, basePath: String = "http://chelona.org", label: String = "") extends ChelonaParser(input: ParserInput, output, validate, basePath, label) with QuadAST {
+
+  import org.chelona.TriGParser.trigWriter
+
+  val trig = new EvalTriG(basePath, label)
+
+  val trigOutput = trigWriter(output)_
+
+  //[1] trigDoc 	::= 	statement*
+  def trigDoc = rule {
+    (statement ~> ((ast: TurtleType) ⇒
+      if (!__inErrorAnalysis) {
+        if (!validate) trig.renderStatement(ast, trigOutput) else
+          ast match {
+            case ASTStatement(ASTComment(s)) ⇒ 0
+            case _                           ⇒ 1
+          }
+      } else 0)).* ~ EOI ~> ((v: Seq[Int]) ⇒ v.foldLeft(0L)(_ + _))
+  }
+
+  //[2] trigDoc 	::= 	directive | block
+  override def statement = rule {
+    (directive | block | comment) ~> ASTStatement
+  }
+
+  //[2g] block	::=	triplesOrGraph | wrappedGraph | triples2 | "GRAPH" labelOrSubject wrappedGraph
+  def block: Rule1[TurtleType] = rule {
+    (triplesOrGraph | wrappedGraph | triples2) ~> ASTBlock |
+      atomic(ignoreCase("graph")) ~ ws ~ labelOrSubject ~ wrappedGraph ~> ASTLabelOrSubjectBlock
+  }
+
+  //[3g]	triplesOrGraph	::=	labelOrSubject (wrappedGraph | predicateObjectList '.')
+  def triplesOrGraph = rule {
+    labelOrSubject ~ (wrappedGraph | predicateObjectList ~ ".") ~> ASTTriplesOrGraph
+  }
+
+  //[4g]	triples2	::=	blankNodePropertyList predicateObjectList? '.' | collection predicateObjectList '.'
+  def triples2 = rule {
+    (blankNodePropertyList ~ predicateObjectList.? ~> ASTTriple2BlankNodePropertyList |
+      collection ~ predicateObjectList ~> ASTTriple2Collection) ~ "."
+  }
+
+  //[5g]	wrappedGraph	::=	'{' triplesBlock? '}'
+  def wrappedGraph = rule {
+    "{" ~ triplesBlock.? ~ "}" ~> ASTWrappedGraph
+  }
+
+  //[6g]	triplesBlock	::=	triples ('.' triplesBlock?)?
+  def triplesBlock = rule {
+    triples.+(('.' ~ ws)) ~ ('.' ~ ws).? ~> ASTTriplesBlock
+  }
+
+  //[7g]	labelOrSubject	::=	iri | BlankNode
+  def labelOrSubject = rule {
+    (iri | blankNode) ~> ASTLabelOrSubject
+  }
+}
+
