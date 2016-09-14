3693083d10833fa2a606a2b841f3f93f650f64f0
jp <juergen.pfundt@gmail.com>
2016-09-15 01:18:42 +0200

    (temporarily?) moved from OutputStream to System.out

--- js/src/main/scala/org/chelona/JSLayer.scala
+++ js/src/main/scala/org/chelona/JSLayer.scala
@@ -1,16 +1,13 @@
 package org.chelona
 
 import java.io._
-import java.nio.charset.StandardCharsets
+import java.nio.charset.{ Charset, StandardCharsets }
 
 import org.chelona.SPOReturnValue.SPOTriple
 import org.parboiled2.{ ErrorFormatter, ParseError, ParserInput }
 
 import scala.util.{ Failure, Success }
 
-/**
- * Created by jp on 15.08.16.
- */
 object JSLayer {
 
   def apply(rdf_input: String, verbose: Boolean = true, validate: Boolean = false) = {
@@ -19,57 +16,19 @@ object JSLayer {
 }
 
 class JSLayer(rdf_input: String, verbose: Boolean, validate: Boolean) {
-  System.out.println("AT BEGINNING OF JSLAYER " + rdf_input)
 
-  //val file = new File(fileName) //cmdLineArgs.get.file
-  //val validate = false // cmdLineArgs.get.validate
-  //val verbose = true // cmdLineArgs.get.verbose
-  /*
-  if (verbose) {
-    System.err.println((if (!validate) "Convert: " else "Validate: ") + file.getCanonicalPath)
-    System.err.flush()
-  }
-*/
   val ms: Double = System.currentTimeMillis
-  /*
-  val inputfile: Try[InputStreamReader] = Try {
-    new InputStreamReader(new FileInputStream (fileName),StandardCharsets.UTF_8 )
-    //io.Source.fromFile(fileName)(StandardCharsets.UTF_8)
-  }
-
-  if (inputfile.isFailure) {
-    System.err.println("Error: " + inputfile.failed.get)
-    sys.exit(3)
-  }
-*/
-  //  val path = Paths.get(fileName)
-  /*
-  val fileChannel: Try[FileChannel] = Try { FileChannel.open(path, util.EnumSet.of(StandardOpenOption.READ)) }
-
-  if (fileChannel.isFailure) {
-    System.err.println("Error: " + fileChannel.failed.get)
-    sys.exit(3)
-  }
-*/
-  //val file = path.toFile.getCanonicalPath
-  /*
-  val f = new FileInputStream (fileName)
-  System.err.println("FileInputStream " + fileName + " size = " + f.getChannel().size())
-*/
-  //System.err.println("File "+file + " size = " + path.toFile.length())
 
   val base = "" // cmdLineArgs.get.base
   val label = if (false /*cmdLineArgs.get.uid*/ ) java.util.UUID.randomUUID.toString.filter((c: Char) ⇒ c != '-').mkString("") else ""
 
   lazy val input: ParserInput = rdf_input
 
-  val output = /*new BufferedWriter(*/ new OutputStreamWriter(System.out, StandardCharsets.UTF_8) /*)*/
+  val output = /*new BufferedWriter(*/ new OutputStreamWriter(System.out, "UTF-8" /*StandardCharsets.UTF_8*/ ) /*)*/
 
   def tripleWriter(bo: Writer)(triple: List[SPOReturnValue]): Int = {
-    System.err.println("triple= " + triple)
-    triple.asInstanceOf[List[SPOTriple]].map(triple ⇒ bo.write(s"${triple.s.text} ${triple.p.text} ${triple.o.text} .\n")).length
+    triple.map { case SPOTriple(TurtleElement(s, l1), TurtleElement(p, l2), TurtleElement(o, l3)) ⇒ { System.out.print(s"${s} ${p} ${o} .\n") } }.length
   }
-  /**/
 
   val file = "chelona.ttl"
   val parser = ChelonaParser(input, tripleWriter(output) _, validate, base, label)
@@ -91,6 +50,4 @@ class JSLayer(rdf_input: String, verbose: Boolean, validate: Boolean) {
     case Failure(e: ParseError) ⇒ if (true /*!cmdLineArgs.get.trace*/ ) System.err.println("File '" + file + "': " + parser.formatError(e)) else System.err.println("File '" + file + "': " + parser.formatError(e, new ErrorFormatter(showTraces = true)))
     case Failure(e)             ⇒ System.err.println("File '" + file + "': Unexpected error during parsing run: " + e)
   }
-
-  System.out.println("EXITING JSLayer")
 }
